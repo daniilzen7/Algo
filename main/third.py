06b9c505840b344489270effcecf4c9ab93c6f13
daniilzen7 <112953466+daniilzen7@users.noreply.github.com>
2022-09-29 13:54:04 +0300

    Update third.py

--- main/third.py
+++ main/third.py
@@ -5,14 +5,14 @@ def numpy_fizzbuzz(N: int) -> str:
     integers = np.arange(N)
     result = np.arange(N).astype(str)
 
-    # Find indices in 'integers' that are divisible by 3 and 5
+    # Находим индексы в 'integers' которые делятся на 3 и 5
     mod_3 = np.argwhere(integers % 3 == 0)
     mod_5 = np.argwhere(integers % 5 == 0)
 
-    # If a number is divisible by 3 and 5, it is also divisible by 15
+    # Если число делится на 3 и 5, то оно делится и на 15
     mod_15 = np.intersect1d(mod_3, mod_5, assume_unique=True)
 
-    # Update results at corresponding indices
+    # Обновляем результаты по соответствующим индексам
     result[mod_3] = 'fizz'
     result[mod_5] = 'buzz'
     result[mod_15] = 'fizzbuzz'
